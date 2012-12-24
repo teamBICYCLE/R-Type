cadc9b4405af170fb0470e1a1294a5338a489427
Loïs Burg <burg.lois@gmail.com>
2012-12-24 12:23:45 +0100

    Added tests for locking, trylocking, locking multiple times. Works on Windows, to be tested on Linux

--- sources/thread_test.cpp
+++ sources/thread_test.cpp
@@ -1,5 +1,5 @@
 #ifdef  __gnu_linux__
-    # include <unistd.h>
+# include <unistd.h>
 #endif
 #include <ctime>
 #include <iostream>
@@ -7,12 +7,13 @@
 #include <queue>
 #include <stdexcept>
 #ifdef  __gnu_linux__
-    # include "UnixMutex.hh"
-    # include "UnixThread.hh"
+# include "UnixMutex.hh"
+# include "UnixThread.hh"
 #elif   _WIN32
-    # include "WinMutex.hh"
-    # include "WinThread.hh"
+# include "WinMutex.hh"
+# include "WinThread.hh"
 #endif
+
 std::queue<std::string>	g_work_queue;
 #ifdef  __gnu_linux__
 IMutex				*g_work_queue_crit_section = new UnixMutex();
@@ -33,12 +34,22 @@ void	fill_work_queue(void)
 	g_work_queue.push("applications mobiles nulles a chier");
 }
 
+void	waitABit()
+{
+#ifdef __gnu_linux__
+		usleep(1000000);
+#elif _WIN32
+		Sleep(1000);
+#endif
+}
+
 void		*thread_routine(void *arg)
 {
 	int		id = *static_cast<int*>(arg);
 	int		*nb_tasks = new int(0);
 	bool	work_available = true;
 
+	waitABit();
 	while (work_available)
 	{
 		g_work_queue_crit_section->lock();
@@ -59,6 +70,90 @@ void		*thread_routine(void *arg)
 	std::cout << "Thread " << id << " : I'm out of here! I did all those " << *nb_tasks << " tasks." << std::endl;
 	return nb_tasks;
 }
+
+void	*lock_test_routine(void *arg)
+{
+	int	id = *static_cast<int*>(arg);
+
+	waitABit();
+	std::cout << "Thread " << id << " locking..." << std::endl;
+	g_work_queue_crit_section->lock();
+	std::cout << "Thread " << id << " locked! Sleeping, then unlocking..." << std::endl;
+	waitABit();
+	std::cout << "Thread " << id << " unlocking..." << std::endl;
+	g_work_queue_crit_section->unlock();
+	return new int(0);
+}
+
+void	*multiple_lock_test_routine(void *arg)
+{
+	int	id = *static_cast<int*>(arg);
+
+	waitABit();
+	std::cout << "Thread " << id << " locking..." << std::endl;
+	g_work_queue_crit_section->lock();
+	std::cout << "Thread " << id << " locked! Sleeping, then locking two times now..." << std::endl;
+	waitABit();
+	g_work_queue_crit_section->lock();
+	g_work_queue_crit_section->lock();
+	std::cout << "Thread " << id << " done! Unlocking and leaving now..." << std::endl;
+	g_work_queue_crit_section->unlock();
+	g_work_queue_crit_section->unlock();
+	g_work_queue_crit_section->unlock();
+	return new int(0);
+}
+
+void	*trylock_test_routine(void *arg)
+{
+	int	id = *static_cast<int*>(arg);
+	
+	waitABit();
+	std::cout << "Thread " << id << " trylocking..." << std::endl;
+	if (g_work_queue_crit_section->trylock())
+	{
+		std::cout << "Success! Sleeping..." << std::endl;
+		waitABit();
+		g_work_queue_crit_section->unlock();
+	}
+	else
+		std::cout << "Failure..." << std::endl;
+	std::cout << "Thread " << id << " leaving!" << std::endl;
+	return new int(0);
+}
+
+#ifdef _WIN32
+void	run_routine(std::function<void*(void*)> routine, WinThread *threads, int nb_threads)
+{
+	for (int i = 0; i < nb_threads; i++)
+	{
+		int *id = new int(i + 1);
+		threads[i].start(routine, id);
+		waitABit();
+	}
+	for (int i = 0; i < nb_threads; i++)
+	{
+		int	*ret = new int(0);
+		threads[i].wait((void**)&ret);
+		std::cout << "Thread " << i + 1 << " returned with " << *ret << std::endl;
+	}
+	std::cout << "----------------------------------------------" << std::endl;
+}
+#elif __gnu_linux__
+void	run_routine(std::function<void*(void*)> routine, UnixThread *threads, int nb_threads)
+{
+	for (int i = 0; i < nb_threads; i++)
+	{
+		int *id = new int(i + 1);
+		threads[i].start(&thread_routine, id);
+	}
+	for (int i = 0; i < nb_threads; i++)
+	{
+		int	*ret = new int(0);
+		threads[i].wait((void**)&ret);
+		std::cout << "Thread " << i + 1 << " returned with " << *ret << std::endl;
+	}
+}
+#endif
 
 int	main(int argc, char *argv[])
 {
@@ -83,17 +178,10 @@ int	main(int argc, char *argv[])
 #elif __gnu_linux__
 		threads = new UnixThread[nb_threads];
 #endif
-		for (int i = 0; i < nb_threads; i++)
-		{
-			int *id = new int(i + 1);
-			threads[i].start(&thread_routine, id);
-		}
-		for (int i = 0; i < nb_threads; i++)
-		{
-			int	*ret = new int(0);
-			threads[i].wait((void**)&ret);
-			std::cout << "Thread " << i + 1 << " returned with " << *ret << std::endl;
-		}
+		run_routine(&thread_routine, threads, nb_threads);
+		run_routine(&trylock_test_routine, threads, nb_threads);
+		run_routine(&lock_test_routine, threads, nb_threads);
+		run_routine(&multiple_lock_test_routine, threads, nb_threads);
 		std::cout << "All work is done!" << std::endl;
 		return EXIT_SUCCESS;
 	} catch (std::exception& error) {
