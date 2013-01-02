7a9bc3c3aa518d6de02f4e5124af3b20c2d662f8
Thomas Duplomb <thomas.duplomb@gmail.com>
2013-01-02 17:18:22 +0100

    Windows CMake configuration
    When compiled on windows, the executable do not spawn terminal window
    anymore.

--- src/rtype/main.cpp
+++ src/rtype/main.cpp
@@ -3,7 +3,22 @@
 #include <system/threads/WinMutex.hh>
 using namespace std;
 
-int main(int argc, char* argv[]) {
+#ifdef _WIN32
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+#endif
+
+#ifdef _WIN32
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine,
+                   int nCmdShow)
+#elif defined UNIX
+int main(int argc, char* argv[]) 
+#endif
+{
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
