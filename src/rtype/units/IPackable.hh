7822dfc49e96dea91ffda92bdebe4df46b6307a1
Zoneur <burg.lois@gmail.com>
2013-01-09 19:45:11 +0100

    Added the IPackable interface for class to inherit when they want to be sent on the network

---
+++ src/rtype/units/IPackable.hh
@@ -0,0 +1,15 @@
+#ifndef     __IPACKABLE_HH__
+# define    __IPACKABLE_HH__
+
+# include   <cstdint>
+
+class   IPackable
+{
+    public:
+        virtual ~IPackable() {}
+
+    public:
+        virtual size_t      pack(uint8_t *out, size_t outSize) const = 0;
+};
+
+#endif /* !__IPACKABLE_HH__ */
