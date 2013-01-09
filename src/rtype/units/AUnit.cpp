85646737a403740146643809663ca913f15971ba
Zoneur <burg.lois@gmail.com>
2013-01-09 19:45:36 +0100

    Modified AUnit and CMakeLists to support the packing

--- src/rtype/units/AUnit.cpp
+++ src/rtype/units/AUnit.cpp
@@ -1,3 +1,5 @@
+#include <cstring>
+#include <stdexcept>
 #include "AUnit.hh"
 
 AUnit::AUnit(int id, const Vector2D& pos, const Vector2D& dir)
@@ -11,6 +13,10 @@ AUnit::AUnit(const AUnit& other)
     : _id(other._id)
     , _pos(other._pos)
     , _dir(other._dir)
+{
+}
+
+AUnit::~AUnit()
 {
 }
 
@@ -32,6 +38,21 @@ void    swap(AUnit& lhs, AUnit& rhs)
     std::swap(lhs._dir, rhs._dir);
 }
 
+int     AUnit::getId(void) const
+{
+    return _id;
+}
+
+const Vector2D& AUnit::getPos(void) const
+{
+    return _pos;
+}
+
+const Vector2D& AUnit::getDir(void) const
+{
+    return _dir;
+}
+
 void    AUnit::move(void)
 {
     _pos += _dir;
@@ -40,6 +61,35 @@ void    AUnit::move(void)
 void    AUnit::setDirection(const Vector2D& dir)
 {
     _dir = dir;
+}
+
+size_t  AUnit::pack(uint8_t *out, size_t outSize) const
+{
+    UnitPacket_u            packetContent;
+
+    packetContent.id = _id;
+    packetContent.x = _pos.x;
+    packetContent.y = _pos.y;
+    packetContent.dx = _dir.x;
+    packetContent.dy = _dir.y;
+
+    network_packet::Packet packet(network_packet::Type::POSITION, packetContent.whole, sizeof(packetContent.whole));
+
+    if (outSize < packet.getDataSize())
+        throw std::overflow_error("Output is too small for the packet to fit");
+    std::memcpy(out, packet.getData(), packet.getDataSize());
+    return packet.getDataSize();
+}
+
+void    AUnit::update(network_packet::Packet packet)
+{
+    const UnitPacket_u *content = reinterpret_cast<const UnitPacket_u*>(packet.getContent());
+
+    _id = content->id;
+    _pos.x = content->x;
+    _pos.y = content->y;
+    _dir.x = content->dx;
+    _dir.y = content->dy;
 }
 
 std::ostream&   operator<<(std::ostream& stream, const AUnit& unit)
--- src/rtype/units/AUnit.hh
+++ src/rtype/units/AUnit.hh
@@ -2,13 +2,28 @@
 # define    __AUNIT_HH__
 
 # include   <ostream>
+# include   "IPackable.hh"
+# include   "Packet.hh"
 # include   "utilities/Vector2D.hh"
 
-class   AUnit
+typedef union {
+    struct {
+        uint32_t    id;
+        uint16_t    x;
+        uint16_t    y;
+        uint16_t    dx;
+        uint16_t    dy;
+    };
+    uint8_t         whole[sizeof(id) + sizeof(x) +
+                          sizeof(y) + sizeof(dx) +
+                          sizeof(dy)];
+} UnitPacket_u;
+
+class   AUnit : public IPackable
 {
     public:
         AUnit(int id, const Vector2D& pos, const Vector2D& dir);
-        virtual ~AUnit() {}
+        virtual ~AUnit();
 
     public:
         AUnit(const AUnit& other);
@@ -17,8 +32,17 @@ class   AUnit
         friend void    swap(AUnit& lhs, AUnit& rhs);
 
     public:
+        int             getId(void) const;
+        const Vector2D& getPos(void) const;
+        const Vector2D& getDir(void) const;
+
+    public:
         void    move(void);
         void    setDirection(const Vector2D& dir);
+        void    update(network_packet::Packet packet);
+
+    public:
+        virtual size_t  pack(uint8_t *out, size_t outSize) const;
 
     public:
         friend std::ostream&    operator<<(std::ostream& stream, const AUnit& unit);
@@ -27,6 +51,7 @@ class   AUnit
         Vector2D    _pos;
         Vector2D    _dir;
         int         _id;
+
 };
 
 #endif /* !__AUNIT_HH__ */
