bc5a3c85a80e40fcb4237aba1251df003fb1bec6
Loadex <carpen_t@epitech.eu>
2013-01-14 11:27:11 +0100

    added sprites gestion

---
+++ src/rtype/sprites/Animation.hh
@@ -0,0 +1,31 @@
+#ifndef  __ANIMATION__
+# define __ANIMATION__
+
+# include <SFML/Graphics.hpp>
+# include <vector>
+# include <chrono>
+
+namespace Sprite
+{
+	class Animation
+	{
+	public:
+		Animation(void) : _duration(0){};
+		Animation(const std::vector<sf::Rect<int>> &sprite, const float duration);
+		Animation(const Animation &other);
+		Animation &operator=(const Animation &other);
+		virtual ~Animation(void);
+
+
+		sf::Rect<int> operator[](const uint position) const;
+		void setRects(const std::vector<sf::Rect<int>> &sprite);
+		void setDuration(const std::chrono::milliseconds duration);
+		std::vector<sf::Rect<int>> getRects(void) const;
+		const std::chrono::milliseconds getDuration(void) const;
+
+	private:
+		std::vector<sf::Rect<int>> _sprite;
+		std::chrono::milliseconds _duration;
+	};
+}
+#endif /*__ANIMATION__*/
