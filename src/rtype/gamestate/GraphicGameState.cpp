e34a12acc91d1968157021df898e7f20b602b521
Zoneur <burg.lois@gmail.com>
2013-01-15 21:40:46 +0100

    Implemented the scrolling starfield
    Also added another method in GraphicGameState: animationUpdate. This is
    where we will update every object's animation. I think.

--- src/rtype/gamestate/GraphicGameState.cpp
+++ src/rtype/gamestate/GraphicGameState.cpp
@@ -15,6 +15,11 @@ using namespace TBSystem;
 
 GraphicGameState::GraphicGameState(const std::vector<std::shared_ptr<Player>>& v)
   : GameState(v)
+  , _backgroundTexture(new sf::Texture)
+  , _backgroundSprite1(new sf::Sprite)
+  , _backgroundSprite2(new sf::Sprite)
+  , _backgroundPos()
+  , _backgroundDirection(-1.f, 0.f)
 {
   using namespace std::placeholders;
 
@@ -22,13 +27,22 @@ GraphicGameState::GraphicGameState(const std::vector<std::shared_ptr<Player>>& v
       std::bind(&GraphicGameState::updateWithPosition, this, _1);
    _updateMap[communication::Packet::Type::DEATH] =
       std::bind(&GraphicGameState::updateWithDeath, this, _1);
+
+  _backgroundTexture->loadFromFile("resources/background.jpg");
+  _backgroundSprite1->setTexture(*_backgroundTexture);
+  _backgroundSprite2->setTexture(*_backgroundTexture);
 }
 
 GraphicGameState::~GraphicGameState()
 {
+  delete _backgroundTexture;
+  delete _backgroundSprite1;
+  delete _backgroundSprite2;
 }
 
 void GraphicGameState::draw(sf::RenderTarget &target, sf::RenderStates states) const {
+  target.draw(*_backgroundSprite1);
+  target.draw(*_backgroundSprite2);
   for (auto& p : _players) {
     target.draw(static_cast<GPlayer&>(*p));
   }
@@ -58,4 +72,20 @@ void  GraphicGameState::simulate(const Input::Data& input)
       GameState::setPlayerDirection(playerId, input.getVector());
     _players[playerId]->move();
   }
+}
+
+void  GraphicGameState::animationUpdate(void)
+{
+  sf::Vector2f pos;
+  
+  _backgroundPos += (_backgroundDirection / GraphicGameState::BACKGROUND_SPEED);
+  pos = static_cast<sf::Vector2f>(_backgroundPos);
+	pos.x *= GameState::WINDOW_WIDTH;
+	pos.y *= GameState::WINDOW_HEIGHT;
+  _backgroundSprite1->setPosition(pos);
+  pos.x += _backgroundTexture->getSize().x;
+  std::cout << "Pos.x=" << pos.x << std::endl;
+  if (pos.x <= 0)//if the x of the second background is less than zero...
+    _backgroundPos.x = 0;//then we reset the position of the first background to loop again
+  _backgroundSprite2->setPosition(pos);
 }
