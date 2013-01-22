2d64d53279fa7c0e870cf0a3d2b0c42d91de51fc
Thomas Duplomb <thomas.duplomb@gmail.com>
2013-01-22 15:30:19 +0100

    Make the standalone server compile

--- src/rtype/server.cpp
+++ src/rtype/server.cpp
@@ -78,16 +78,16 @@ void  runServer(const std::vector<std::string>& clientsIps,
   }
 }
 
-//int     main(int argc, char *argv[])
-//{
-  //std::vector<std::string>  clients;
+int     main(int argc, char *argv[])
+{
+  std::vector<std::string>  clients;
 
-  //clients.push_back("192.168.1.32");
-  //clients.push_back("10.23.98.230");
-  //clients.emplace_back("10.23.99.201");
-  //clients.emplace_back("10.23.99.200");
-  //clients.emplace_back("10.23.98.230");
-  //clients.push_back("10.23.98.165");
-  //runServer(clients, "4242");
-  //return EXIT_SUCCESS;
-//}
+  clients.push_back("192.168.1.32");
+  clients.push_back("10.23.98.230");
+  clients.emplace_back("10.23.99.201");
+  clients.emplace_back("10.23.99.200");
+  clients.emplace_back("10.23.98.230");
+  clients.push_back("10.23.98.165");
+  runServer(clients, "4242");
+  return EXIT_SUCCESS;
+}
