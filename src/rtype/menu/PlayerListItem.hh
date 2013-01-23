05359e87dbb1edf1d2c9d82dc260f30a700f0a79
root <root@geoffroy-HP-EliteBook-8540p.(none)>
2013-01-23 02:22:54 +0100

    UI customization

--- src/rtype/menu/PlayerListItem.hh
+++ src/rtype/menu/PlayerListItem.hh
@@ -9,10 +9,12 @@
 class PlayerListItem : public ListItem {
 private:
   sf::Text		_status;
+  sf::RectangleShape	_color;
 
 public:
   PlayerListItem(const sf::Vector2f& dimension, const std::string& id,
-		 const std::string& status = "EMPTY", const std::string& texture = DEFAULT_LISTITEM_TEXTURE);
+		 const std::string& status = "EMPTY", const sf::Color& color = sf::Color::Black,
+		 const std::string& texture = DEFAULT_LISTITEM_TEXTURE);
   virtual ~PlayerListItem();
 
   virtual void		draw(sf::RenderTarget& target);
--- src/rtype/menu/View.cpp
+++ src/rtype/menu/View.cpp
@@ -8,13 +8,16 @@ View::View(const std::string& texture, const sf::Vector2u& size, const sf::Vecto
 {
   float x,y;
 
+  _logoTexture.loadFromFile(LOGO_PATH);
+  _logo.setTexture(_logoTexture);
+  _logo.setPosition(sf::Vector2f(120,60));
   _texture.loadFromFile(texture);
   _background.setTexture(_texture);
   _background.setPosition(size.x, size.y);
   _background.setOrigin(1920 / 2, 1920 / 2);
   _background.scale(1.8,1.8);
   x = (_size.x / 2) - (listItemSize.x / 2);
-  y = (_size.y / 2) - (MAX_ROW * listItemSize.y / 2);
+  y = (_size.y / 2) - ((MAX_ROW - 1)  * listItemSize.y / 2);
   _listView = new ListView(sf::Vector2f(x,y), listItemSize);
   _title.setCharacterSize(18);
   _title.setStyle(0);
@@ -43,6 +46,7 @@ void	View::draw(sf::RenderWindow& target)
 {
   _background.rotate(0.015);
   target.draw(_background);
+  target.draw(_logo);
   target.draw(_title);
   _listView->draw(target);
   if (_buttonOne)
